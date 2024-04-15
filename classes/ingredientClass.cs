0b01436836a91fdbd070db291f389ec2bbe7954d
SkarbrandtheExiled <131450052+SkarbrandtheExiled@users.noreply.github.com>
2024-04-15 17:52:39 +0200

    added some exception handlers to prevent the application from crashing. As well as adding some commments to explain some of the code

--- classes/ingredientClass.cs
+++ classes/ingredientClass.cs
@@ -8,15 +8,16 @@ namespace LukeC_ST10254164_PROG6221_part1.classes
 {
     internal class ingredientClass
     {
-        private string[] ingredientNames;
-        private double[] ingredientQuantities;
-        private string[] unitOfMeasurements;
+        //-------initalisation of arrays that will be used to store user data------//
+        private string[] ingredientNames; //an array that must store the names of the ingredients (with the data type string)
+        private double[] ingredientQuantities; //an array that stores the quantity/ number of those ingredients (data type is double to ensure users can include fractions)
+        private string[] unitOfMeasurements; // an array that stores the unit of measurments for the ingredients (data type is a string because some recipes don't use numbers to indicate measurements)
         private List<string> steps;
         //---------------------------Ingrediants method----------------------------//
         public void ingredients()
         {
-            Console.WriteLine("Enter the number of ingredients: ");
-            int numIngredients = int.Parse( Console.ReadLine() );
+            Console.WriteLine("Please enter the number of ingredients: ");
+            int numIngredients = int.Parse( Console.ReadLine() ); //must fix this to have error handling
 
             ingredientNames = new string[numIngredients];
             ingredientQuantities = new double[numIngredients];
@@ -24,30 +25,49 @@ namespace LukeC_ST10254164_PROG6221_part1.classes
 
             for (int i = 0; i < numIngredients; i++)
             {
-                Console.WriteLine($"Enter the name of ingredient {i + 1}: ");
+                Console.WriteLine($"Please enter the name of ingredient {i + 1}: ");
                 ingredientNames[i] = Console.ReadLine();
 
-                Console.WriteLine($"Please enter the quantity of {ingredientNames[i]}: ");
-                ingredientQuantities[i] = double.Parse( Console.ReadLine() );
+                if (!string.IsNullOrWhiteSpace(ingredientNames[i]))
+                {
+                    Console.WriteLine($"Please enter the quantity of {ingredientNames[i]}: ");
 
-                Console.WriteLine($"Enter the unit of measurement for {ingredientNames[i]}: ");
-                unitOfMeasurements[i] = Console.ReadLine();
+                    try
+                    {
+                        ingredientQuantities[i] = double.Parse(Console.ReadLine());
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number for the quantity.");
+                        i--; // Decrement i to repeat the same index
+                        continue;
+                    }
+
+                    Console.WriteLine($"Please enter the unit of measurement for {ingredientNames[i]}: ");
+                    unitOfMeasurements[i] = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid name for the ingredient.");
+                    i--; // Decrement i to repeat the same index
+                }
             }
 
-            Console.WriteLine("Enter the number of steps: ");
+            Console.WriteLine("Please enter the number of steps: ");
             int numSteps = int.Parse( Console.ReadLine());
 
             steps = new List<string>(numSteps);
             for (int i = 0; i < numSteps; i++)
             {
-                Console.WriteLine($"Enter step {i + 1}: ");
+                Console.WriteLine($"Please enter step {i + 1}: ");
                 steps.Add(Console.ReadLine());
             }
             displayRecipe();
         }
+//----------------------------displayRecipe method----------------------------------//
         public void displayRecipe()
         {
-            Console.WriteLine("Recipe:");
+            Console.WriteLine("\nfull recipe:");
 
             Console.WriteLine("Ingredients:");
             for (int i = 0; i < ingredientNames.Length; i++)
@@ -55,11 +75,18 @@ namespace LukeC_ST10254164_PROG6221_part1.classes
                 Console.WriteLine($"{ingredientQuantities[i]} {unitOfMeasurements[i]} of {ingredientNames[i]}");
             }
 
-            Console.WriteLine("Steps:");
+            Console.WriteLine("\nSteps:");
             for (int i = 0; i < steps.Count; i++)
             {
-                Console.WriteLine($"{i + 1} {steps[i]}");
+                Console.WriteLine($"{i + 1}: {steps[i]}");
             }
+            Console.WriteLine("press 'enter' to quite application");
+            Console.ReadLine();
+        }
+        //--------------------deleteData method---------------------------//
+        public void deleteData() //method must delete the data inputted by the user and loop the application back to the start
+        {
+
         }
     }
 }
